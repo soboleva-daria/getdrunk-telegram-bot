4fe476dbcaf526a9ddc367fa5908d7aecae30653
skachkov-nikolay <nikolaj-skachkov@yandex.ru>
2020-05-11 15:11:27 +0300

    Refactoring. Message is handled according to chat_id

--- bot/server.py
+++ bot/server.py
@@ -1,57 +1,28 @@
-import argparse
 import requests
+import textwrap
+import random
+import pickle
+import json
+import pandas as pd
 
 from flask import request, Flask
 from string import punctuation
-
-# TODO: fixme later
-import sys
-sys.path.append('../')
+from datetime import datetime
 
 from model.predict import BaseModel, TFIdfCocktailModel, BertCocktailModel
-import pandas as pd
 
 from drinks.cocktail import Cocktail
-import random
-import pickle
-from datetime import datetime
-import numpy as np
 
 
-class GetDrunkTelegramBot:
-    # TODO: add exploratory user request
-    # TODO: Better formatting for special names? cocktail name etc.
-    # TODO: more information for user about what cocktail is currently processing.
-    # TODO: user should have an opportunity to provide bac in the begnning and maybe weight?
-    def __init__(
-            self,
-            token,
-            hook_url,
-            model_name='BaseModel',
-            train=None,
-            model_config_file=None,
-            model_vocab_file=None,
-            debug=False
-    ):
+class TelegramInterface:
+    def __init__(self, token, hook_url, debug=False):
+        if debug:
+            print('Starting tg interface...')
         self._bot_url = f"https://api.telegram.org/bot{token}"
         self._set_web_hook(hook_url)
         self._chat_id = None
-
-        self.model_name = model_name
-        self.train = train
-        self.model_config_file = model_config_file
-        self.model_vocab_file = model_vocab_file
-        self.model = None
-        self.init_model()
-
-        self.cocktail = None
-        self.total_alcohol_absorbed = 0
-        self.cocktails_history = []
-        self.recipes_of_the_day = self.load_recipes_of_the_day()
-        self.index = None # index of the cocktail in recipe of the day list, refactor here
-
-        self.debug = debug
-        self.init_model()
+        if debug:
+            print('tg interface started successfully.')
 
     def _set_web_hook(self, hook_url):
         method = "setWebhook"
@@ -59,20 +30,55 @@ class GetDrunkTelegramBot:
         data = {"url": hook_url}
         requests.post(url, data=data)
 
-    def _send_message(self, text, photo=False, photo_path=None):
-        if photo:
-            method = "sendPhoto"
-            url = f"{self._bot_url}/{method}"
-            data = {"chat_id": self._chat_id, 'caption': text}
-            with open(photo_path, "rb") as image_file:
-                requests.post(url, data=data, files={"photo": image_file})
-        else:
-            method = "sendMessage"
-            url = f"{self._bot_url}/{method}"
-            data = {"chat_id": self._chat_id, "text": text}
-            requests.post(url, data=data)
+    def _send_photo(self, chat_id, text, photo_path):
+        method = "sendPhoto"
+        url = f"{self._bot_url}/{method}"
+        data = {"chat_id": chat_id, 'caption': text}
+        with open(photo_path, "rb") as image_file:
+            requests.post(url, data=data, files={"photo": image_file})
 
-    def init_model(self):
+    def _send_message(self, chat_id, text):
+        method = "sendMessage"
+        url = f"{self._bot_url}/{method}"
+        data = {"chat_id": chat_id, "text": text}
+        requests.post(url, data=data)
+
+
+class GetDrunkBotHandler(TelegramInterface):
+    # TODO: add exploratory user request
+    # TODO: Better formatting for special names? cocktail name etc.
+    # TODO: more information for user about what cocktail is currently processing.
+    # TODO: user should have an opportunity to provide bac in the begnning and maybe weight?
+    def __init__(
+            self,
+            model_name='BaseModel',
+            train=None,
+            model_config_file=None,
+            model_vocab_file=None,
+            debug=False,
+            **telegram_kwargs
+    ):
+        super(GetDrunkBotHandler, self).__init__(
+            **telegram_kwargs, debug=debug)
+
+        self.model_name = model_name
+        self.train = train
+        self.model_config_file = model_config_file
+        self.model_vocab_file = model_vocab_file
+        self.model = None
+        self._create_model()
+
+        self.cocktail = None
+
+        self.total_alcohol_absorbed = 0
+        self.cocktails_history = []
+        self.recipes_of_the_day = self.load_recipes_of_the_day()
+        self.index = None # index of the cocktail in recipe of the day list, refactor here
+
+        self.debug = debug
+        self._create_model()
+
+    def _create_model(self):
         if self.model_name == 'BaseModel':
             self.model = BaseModel()
         elif self.model_name == 'TFIdfCocktailModel':
@@ -81,112 +87,108 @@ class GetDrunkTelegramBot:
             self.model = BertCocktailModel(self.model_config_file, self.model_vocab_file)
         else:
             raise ValueError(
-                "Error in model_name. Available models: {}, Got: {}".format(
-                    ['TFIdfCocktailModel', 'BertCocktailModel', 'BaseModel'],
-                    self.model_name))
+                f"Error in model_name. Available models: "
+                f"{['TFIdfCocktailModel', 'BertCocktailModel', 'BaseModel']}, "
+                f"Got: {self.model_name}")
 
     def process_message(self, chat_id, msg):
-        self._chat_id = chat_id
-
         if self.debug:
             print("Got a message: <%s>." % msg)
 
         if msg == '\start'.strip():
-            self._start_session_and_say_hello()
+            self._start_session_and_say_hello(chat_id)
 
         elif msg == '\end':
-            self._end_session_and_say_bye()
+            self._end_session_and_say_bye(chat_id)
 
         elif msg == '\\recipe of the day':
-            self._send_day_cocktail()
+            self._send_day_cocktail(chat_id)
 
         elif '\\recipe' in msg:
             ingredients = self.parse_ingredients(msg)
-            self._send_best_cocktail_with_ingredients(ingredients)
+            self._send_best_cocktail_with_ingredients(chat_id, ingredients)
 
         elif msg == '\photo':
-            self._send_cocktail_image(self.cocktail)
+            self._send_cocktail_image(chat_id, self.cocktail)
 
         elif msg == '\intoxication level':
-            self._send_intoxication_degree()
+            self._send_intoxication_degree(chat_id)
 
         elif msg == '\info':
-            self._send_cocktail_useful_info(self.cocktail)
+            self._send_cocktail_useful_info(chat_id, self.cocktail)
 
         elif msg == '\menu':
-            self._send_cocktails_menu()
+            self._send_cocktails_menu(chat_id)
 
         elif '\explore' in msg:
             ingredients = self.parse_ingredients(msg)
-            self._send_exploration_result(ingredients)
+            self._send_exploration_result(chat_id, ingredients)
         else:
-            self._send_help_message()
+            self._send_help_message(chat_id)
 
-    def _start_session_and_say_hello(self):
+    def _start_session_and_say_hello(self, chat_id):
         self.total_alcohol_absorbed = 0
-        msg = \
-            """
-Hey there, wanna get drunk? 💫
+        msg = textwrap.dedent("""
+            Hey there, wanna get drunk? 💫
+            
+            Here is what I can do for you:
+            
+            — find cocktail recipe with your ingredients 🥂
+            
+            — tell your current intoxication level 🐙
+            
+            — provide recipe of the day 💜
+            
+            — explore cocktails for you  💻
+        """)
+        self._send_message(chat_id, msg)
 
-Here is what I can do for you:
+    def _end_session_and_say_bye(self, chat_id):
+        msg = textwrap.dedent("""
+            You’re welcome anytime! ❤️
+            Bye 🥂
+        """)
+        self._send_message(chat_id, msg)
 
-— find cocktail recipe with your ingredients 🥂
-
-— tell your current intoxication level 🐙
-
-— provide recipe of the day 💜
-
-— explore cocktails for you  💻
-            """
-        self._send_message(msg)
-
-    def _end_session_and_say_bye(self):
-        msg = \
-            """
-You’re welcome anytime! ❤️
-Bye 🥂
-            """
-        self._send_message(msg)
-
-    def _send_best_cocktail_with_ingredients(self, ingredients):
+    def _send_best_cocktail_with_ingredients(self, chat_id, ingredients):
         self.cocktail = self.model.predict(ingredients)
-        msg = \
-            """
-{}
-
-Ingredients: {}
-
-Method: {}  
-
-
-Enjoy! 💫
-        """.format(self.cocktail.name, ', '.join(self.cocktail.ingredients).strip(), self.cocktail.recipe)
+        msg = textwrap.dedent(f"""
+            { self.cocktail.name }
+            
+            Ingredients: { ', '.join(self.cocktail.ingredients).strip() }
+            
+            Method: { self.cocktail.recipe }  
+            
+            Enjoy! 💫
+        """)
         self.total_alcohol_absorbed += self.cocktail.abv * self.cocktail.volume
         self.cocktails_history.append(self.cocktail)
-        self._send_message(msg)
+        self._send_message(chat_id, msg)
 
-    def _send_cocktail_image(self, cocktail):
+    def _send_cocktail_image(self, chat_id, cocktail):
         if cocktail is None:
             msg = "Oh 🤗 looks like you didn't select the cocktail. " \
                   "Let's try again, just say \\recipe!"
             self._send_message(msg)
         else:
             # TODO: fix here to send real image
-            msg = \
-                """
-                {}""".format(cocktail.name)
-            self._send_message(msg, photo=True, photo_path='../utils/{}.png'.format(cocktail.orig_name))
+            msg = textwrap.dedent(f"""
+                { cocktail.name }
+            """)
+            self._send_photo(chat_id, msg,
+                             photo_path=f'utils/{cocktail.orig_name}.png')
 
-    def _send_intoxication_degree(self):
+    def _send_intoxication_degree(self, chat_id):
+        cocktail_list = '\n'.join(
+            [cocktail.name for cocktail in self.cocktails_history])
         degree = self._get_intoxication_degree()
-        msg = \
-"""
-You are in {}. 
-
-Here is the list of what you took: 
-{} 
-""".format(degree, '\n'.join([cocktail.name for cocktail in self.cocktails_history]))
-        self._send_message(msg)
+        msg = textwrap.dedent(f"""
+            You are in {degree}. 
+            
+            Here is the list of what you took: 
+            {cocktail_list}
+        """)
+        self._send_message(chat_id, msg)
 
     def _get_intoxication_degree(self):
         # 170 is avg female weight in the US in pounds, TODO: fix here
@@ -209,64 +211,61 @@ Here is the list of what you took:
         else:
             return 'Death 💀 stage (7 out of 7). I will miss you 🥺'
 
-    def _send_cocktail_useful_info(self, cocktail):
+    def _send_cocktail_useful_info(self, chat_id, cocktail):
         if cocktail is None:
             msg = "Oh 🤗 looks like you didn't select the cocktail. " \
                   "Let's try again, just say \\recipe!"
         else:
-            msg = \
-    """
-    {} {}! 🔬
-    """.format(cocktail.name, cocktail.useful_info)
-        self._send_message(msg)
+            msg = textwrap.dedent(f"""
+                {cocktail.name} {cocktail.useful_info}! 🔬
+            """)
+        self._send_message(chat_id, msg)
 
-    def _send_day_cocktail(self):
+    def _send_day_cocktail(self, chat_id):
         weekday_name = datetime.today().strftime("%A")
 
         if self.index is None:
             self.index = random.randint(0, len(self.recipes_of_the_day))
         self.cocktail = self.recipes_of_the_day[self.index]
-        msg = \
-            """
-Our {} menu 👩‍🍳🥳:
-
-{}
-
-Ingredients: {}
-
-Method: {}  
-
-
-Enjoy! 💫
-        """.format(weekday_name, self.cocktail.name, ', '.join(self.cocktail.ingredients).strip(), self.cocktail.recipe)
+        msg = textwrap.dedent(f"""
+            Our {weekday_name} menu 👩‍🍳🥳:
+            
+            {self.cocktail.name}
+            
+            Ingredients: {', '.join(self.cocktail.ingredients).strip()}
+            
+            Method: {self.cocktail.recipe}  
+                    
+            Enjoy! 💫
+        """)
 
         self.total_alcohol_absorbed += self.cocktail.abv * self.cocktail.volume
         self.cocktails_history.append(self.cocktail)
-        self._send_message(msg)
+        self._send_message(chat_id, msg)
 
-    def _send_cocktails_menu(self):
-        msg = \
-"""
-Menu 🍽️ 😋: 
+    def _send_cocktails_menu(self, chat_id):
+        cocktail_list = '\n'.join([
+            cocktail.name for cocktail in self.recipes_of_the_day])
+        msg = textwrap.dedent(f"""
+            Menu 🍽️ 😋: 
+            
+            {cocktail_list} 
+        """)
+        self._send_message(chat_id, msg)
 
-{} 
-""".format('\n'.join([cocktail.name for cocktail in self.recipes_of_the_day]))
-        self._send_message(msg)
+    def _send_help_message(self, chat_id):
+        msg = textwrap.dedent("""
+            I am sorry :( I did not get what you mean.
+            
+            Please try again with these commands: 
+            `\start`, `\end`, `\\recipe`, `\photo`, `\intoxication level`, `\\recipe of the day`, `\explore`, `\info`, `\menu`
+            
+            Thank you! 🙏
+        """)
 
-    def _send_help_message(self):
-        msg = \
-"""
-I am sorry :( I did not get what you mean.
+        self._send_message(chat_id, msg)
 
-Please try again with these commands: 
-`\start`, `\end`, `\\recipe`, `\photo`, `\intoxication level`, `\\recipe of the day`, `\explore`, `\info`, `\menu`
-
-Thank you! 🙏
-"""
-
-        self._send_message(msg)
-
-    def _send_exploration_result(self):
+    def _send_exploration_result(self, chat_id):
         pass
 
     # TODO: do we need these methods?
@@ -298,10 +297,11 @@ Thank you! 🙏
     # TODO: put this method into drinks/preprocessing later?
     @staticmethod
     def load_recipes_of_the_day():
-        data = pd.read_csv('../utils/05-CocktailRecipes.csv')[
-            ['RecipeName', 'Ingredients', 'Preparation', 'IMAGE', 'ABV', 'VOLUME', 'AUTHOR', 'LOCATION', 'OriginalRecipeSource']
+        data = pd.read_csv('utils/05-CocktailRecipes.csv')[
+            ['RecipeName', 'Ingredients', 'Preparation', 'IMAGE', 'ABV',
+             'VOLUME', 'AUTHOR', 'LOCATION', 'OriginalRecipeSource']
         ]
-        with open('../utils/emoji.pkl', 'rb') as fin:
+        with open('utils/emoji.pkl', 'rb') as fin:
             emojis = pickle.load(fin)
 
         recipes = []
@@ -316,8 +316,12 @@ Thank you! 🙏
             author = str(author).strip()
             location = str(location).strip()
 
-            useful_info = "was officially invented by the bartender named {} in {}".format(author, location) if author != 'nan' else\
-                "was officially found in the source: {}".format(source)
+            useful_info = (
+                f"was officially invented by the bartender "
+                f"named {author} in {location}"
+                if author != 'nan'
+                else f"was officially found in the source: {source}"
+            )
 
             abv = float(abv)
             volume = float(volume)
@@ -326,20 +330,11 @@ Thank you! 🙏
         return recipes
 
 
-def parse_server_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--port', type=int, default='8888')
-    parser.add_argument('--token', type=str, required=True)
-    parser.add_argument('--web-hook-url', type=str, required=True)
-    parser.add_argument('--debug', type=bool, default=False, nargs='?')
-    return parser.parse_args()
-
-
 def create_server(args):
     app = Flask(__name__)
-
-    get_drunk_bot = GetDrunkTelegramBot(
-        token=args.token, hook_url=args.web_hook_url
+    print('Init Bot')
+    get_drunk_bot = GetDrunkBotHandler(
+        token=args.token, hook_url=args.web_hook_url, debug=args.debug
     )
 
     @app.route('/', methods=['GET', 'POST'])
@@ -354,7 +349,7 @@ def create_server(args):
                 chat_id = data["message"]["chat"]["id"]
                 text = data["message"]["text"]
                 get_drunk_bot.process_message(chat_id, text)
-            except ValueError:
+            except KeyError:
                 pass
         else:
             return "Hello, world!"
@@ -362,11 +357,3 @@ def create_server(args):
         return {"ok": True}
 
     return app
-
-
-if __name__ == '__main__':
-    args = parse_server_args()
-    app = create_server(args)
-
-    # use_reloader is false due to problems with CUDA and multiprocessing
-    app.run(port=args.port, debug=True, use_reloader=False)
