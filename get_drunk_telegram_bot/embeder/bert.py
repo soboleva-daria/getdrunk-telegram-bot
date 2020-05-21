692f297dee206ac43b336bee55f36d5f3b624123
m.yavorskaya <m.yavorskaya@tinkoff.ru>
2020-05-21 00:02:56 +0000

    fix bert embeder

--- get_drunk_telegram_bot/embeder/bert.py
+++ get_drunk_telegram_bot/embeder/bert.py
@@ -26,8 +26,8 @@ class BertEmbeder(IEmbeder):
 
     def tokenize(self, text: str):
         tokenized_text = self.tokenizer.tokenize(text)
-        return ['CLS'] + tokenized_text + ['SEP'] + \
-               ['PAD'] * (self.max_sequence_len - len(tokenized_text) - 2)
+        return ['[CLS]'] + tokenized_text + ['[SEP]'] + \
+               ['[PAD]'] * (self.max_sequence_len - len(tokenized_text) - 2)
 
     def encode(self, tokenized_text: List):
         return self.tokenizer.convert_tokens_to_ids(tokenized_text)
@@ -38,9 +38,9 @@ class BertEmbeder(IEmbeder):
         for i in range(0, len(data), self.batch_size):
             tokens_tensor = torch.tensor([
                             self.encode(self.tokenize(text))
-                            for text in data[i:i + batch_size]
+                            for text in data[i:i + self.batch_size]
                             ])
             encoded_layers, _ = self.model(tokens_tensor)
             batches.append(encoded_layers[-1][:, 0, :])
 
-        return torch.cat(batches).numpy()
+        return torch.cat(batches).detach().numpy()
